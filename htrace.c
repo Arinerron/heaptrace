ef4dca042d47e562ff7b4c67b2e5aef10cd96b61
Aaron Esau <git@aaronesau.com>
2020-12-02 11:43:12 -0800

    Add colors

--- htrace.c
+++ htrace.c
@@ -6,23 +6,27 @@
 
 #define log(f_, ...) fprintf(stderr, (f_), ##__VA_ARGS__)
 
+#define COLOR_RED "\e[0;31m"
+#define COLOR_RED_BOLD "\e[1;31m"
+#define COLOR_RESET "\e[0m"
+
 void *(*orig_malloc)(size_t size);
 void (*orig_free)(void *ptr);
  
 void *malloc(size_t size) {
-    log("... malloc(0x%02lx)\t", size);
+    log("%s... malloc(%s0x%02lx%s)\t%s", COLOR_RED, COLOR_RED_BOLD, size, COLOR_RED, COLOR_RESET);
     void *ptr = orig_malloc(size);
-    log(" = 0x%llx\n", (long long unsigned int)ptr);
+    log("%s = %s0x%llx%s\n", COLOR_RED, COLOR_RED_BOLD, (long long unsigned int)ptr, COLOR_RESET);
     return ptr;
 }
 
 void free(void *ptr) {
-    log("... free(0x%llx)\n", (long long unsigned int)ptr);
+    log("%s... free(%s0x%llx%s)%s\n", COLOR_RED, COLOR_RED_BOLD, (long long unsigned int)ptr, COLOR_RED, COLOR_RESET);
     orig_free(ptr);
 }
  
 void _init(void) {
     if (!orig_malloc) orig_malloc = dlsym(RTLD_NEXT, "malloc");
     if (!orig_free) orig_free = dlsym(RTLD_NEXT, "free");
-    log("Initialized heaptrace.\n");
+    log("%sInitialized heaptrace.%s\n", COLOR_RED, COLOR_RESET);
 }
