c7aef53def4d327e6a6c0857705cb3bec8d56fa0
Aaron Esau <git@aaronesau.com>
2021-10-07 23:21:14 -0700

    gracefully handle missing files / bad perms

--- src/main.c
+++ src/main.c
@@ -27,9 +27,22 @@ int main(int argc, char *argv[]) {
     }
     chargv[argc - start_at] = 0;
 
-    log(COLOR_LOG "================================ " COLOR_LOG_BOLD "BEGIN HEAPTRACE" COLOR_LOG " ===============================\n" COLOR_RESET "\n");
-    // TODO: parse args
+    if (access(chargv[0], F_OK) != 0) {
+        fatal("unable to execute \"%s\": file does not exist.\n", chargv[0]);
+        log(COLOR_WARN "hint: are you sure you specified the correct filename?\n" COLOR_RESET);
+        exit(1);
+    } else if (access(chargv[0], R_OK) != 0) {
+        fatal("permission to read \"%s\" denied.\n", chargv[0]);
+        log(COLOR_WARN "hint: chmod +r %s\n" COLOR_RESET, chargv[0]);
+        exit(1);
+    } else if (access(chargv[0], X_OK) != 0) {
+        fatal("permission to execute \"%s\" denied.\n", chargv[0]);
+        log(COLOR_WARN "hint: chmod +x %s\n" COLOR_RESET, chargv[0]);
+        exit(1);
+    }
 
+    log(COLOR_LOG "================================ " COLOR_LOG_BOLD "BEGIN HEAPTRACE" COLOR_LOG " ===============================\n" COLOR_RESET "\n");
+    
     start_debugger(chargv);
     return 0;
 }
